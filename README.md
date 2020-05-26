9db13e44a689470d93a1d40c0c5c97b9e4d22488
Boris <borochief@gmail.com>
2020-05-26 11:54:54 +0200

    rewrite

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Discord auto-renamer bot
+# Discord auto-channel-renamer bot
 
 A Discord bot that adds the currently most played game of the users inside a voice channel to the channel name.
 
@@ -9,4 +9,5 @@ A Discord bot that adds the currently most played game of the users inside a voi
 
 ## Notes:
 - Don't forget to give the bot persmission to manage the channels.
-- If you want to exclude a channel from being renamed, just remove the manage channel permission for that specific channel.
+- If you want to exclude a channel from being renamed, just remove the bots manage channel permission for that specific channel.
+- If the bot sets the channel name like "xxx (different games)" it means that there is no most played game inside that channel but everyone plays something different
--- autorenamer.py
+++ autorenamer.py
@@ -1,17 +1,17 @@
 import traceback
-
 import re
-
 from collections import Counter
-
 from discord import Forbidden
 from discord.ext import commands
 
-##Configure the bot
-description = '''Gamerbot available Commands: '''
+# Configure the bot
+# --------------------------------------------
+TOKEN = 'YourBotKey'
+description = '''Channel renamer bot '''
 bot = commands.Bot(command_prefix='!', description=description)
 
-client_loop = bot.loop
+
+# --------------------------------------------
 
 
 @bot.event
@@ -25,9 +25,9 @@ async def on_ready():
 @bot.event
 async def on_command_error(error, ctx):
     if isinstance(error, commands.NoPrivateMessage):
-        await bot.send_message(ctx.message.author, 'This command cannot be used in private messages.')
+        await ctx.send('This command cannot be used in private messages.')
     elif isinstance(error, commands.DisabledCommand):
-        await bot.send_message(ctx.message.author, 'Sorry. This command is disabled and cannot be used.')
+        await ctx.send('Sorry. This command is disabled and cannot be used.')
     elif isinstance(error, commands.CommandInvokeError):
         print('In {0.command.qualified_name}:'.format(ctx))
         traceback.print_tb(error.original.__traceback__)
@@ -36,112 +36,59 @@ async def on_command_error(error, ctx):
 
 @bot.event
 async def on_member_update(before, after):
-    if before.game != after.game:
-        if after.voice_channel:
-            try:
-                if not re.search(r'\(.*?\)', after.voice_channel.name):
-                    if after.game:
-                        print(after.display_name + " changed game status to " + after.game.name)
-                        await bot.edit_channel(after.voice_channel, name=after.voice_channel.name + " (" + after.game.name + ")")
-                        print("Changing channel name to: " + after.voice_channel.name + " (" + after.game.name + ")")
-                    else:
-                        print(after.display_name + " changed game status to none")
-                        await bot.edit_channel(after.voice_channel, name=re.sub(r'\(.*?\)', '', after.voice_channel.name))
-                else:
-                    to_name = getMostPlayedGameInChannel(after.voice_channel)
-                    if to_name:
-                        await bot.edit_channel(after.voice_channel, name=re.sub(r'\(.*?\)', '(' + to_name + ')', after.voice_channel.name))
-                        print("Changing channel game tag from " + after.voice_channel.name + " to (" + to_name + ")")
-                    else:
-                        await bot.edit_channel(after.voice_channel, name=re.sub(r'\(.*?\)', '', after.voice_channel.name))
-                        print("Removing game tag from " + after.voice_channel.name + " because most played game is none")
-            except Forbidden:
-                print("Warning: Didn't have permission to change the channel name...")
-        print(after.display_name + " changed game status to none")
+    if before.activities == after.activities:
+        return
+
+    if not after.voice:
+        return
+
+    await handle_game_activity_update(after, after.voice.channel)
 
 
 @bot.event
-async def on_voice_state_update(before, after):
+async def on_voice_state_update(member, before, after):
+    if before.channel:  # update the channel he just left
+        await handle_game_activity_update(member, before.channel)
+
+    if after.channel:  # update the channel he just joined
+        await handle_game_activity_update(member, after.channel)
+
+
+async def handle_game_activity_update(member, voice_channel):
     try:
-        if not before.voice_channel and after.voice_channel:
-            print(after.display_name + " joined voice channel: " + after.voice_channel.name)
-            if after.game:
-                # check if hasn't been named already
-                if not re.search(r'\(.*?\)', after.voice_channel.name):
-                    await bot.edit_channel(after.voice_channel, name=after.voice_channel.name + " (" + after.game.name + ")")
-                    print("Changing channel name to: " + after.voice_channel.name + " (" + after.game.name + ")")
-                else:
-                    to_name = getMostPlayedGameInChannel(after.voice_channel)
-                    if to_name:
-                        await bot.edit_channel(after.voice_channel, name=re.sub(r'\(.*?\)', '(' + to_name + ')', after.voice_channel.name))
-                        print("Changing channel name to: " + after.voice_channel.name + " (" + to_name + ")")
-                    else:
-                        await bot.edit_channel(after.voice_channel, name=re.sub(r'\(.*?\)', '', after.voice_channel.name))
-                        print("Removing eventual game tag from channel " + before.voice_channel.name + " because most played game is none")
+        p = re.compile('(.*)\s\((.*?)\)')
+        m = p.search(voice_channel.name)
 
-        elif before.voice_channel and after.voice_channel and before.voice_channel != after.voice_channel:
-            print(after.display_name + " changed voice channel from " + before.voice_channel.name + " to " + after.voice_channel.name)
-            # remove game from channel name when leaving it and if channel is empty (the brackets)
-            if not before.voice_channel.voice_members:
-                await bot.edit_channel(before.voice_channel, name=re.sub(r'\(.*?\)', '', before.voice_channel.name))
-                print("Removing eventual game tag from empty channel " + before.voice_channel.name)
-            else:
-                # channel that has been left still has people in it. update the name
-                to_name = getMostPlayedGameInChannel(before.voice_channel)
-                if to_name:
-                    if not re.search(r'\(.*?\)', before.voice_channel.name):
-                        await bot.edit_channel(before.voice_channel, name=before.voice_channel.name + " (" + to_name + ")")
-                        print("Changing channel name to " + before.voice_channel.name + " (" + to_name + ")")
-                    else:
-                        await bot.edit_channel(before.voice_channel, name=re.sub(r'\(.*?\)', '(' + to_name + ')', before.voice_channel.name))
-                        print("Changing game tag from " + before.voice_channel.name + " to (" + to_name + ")")
-                else:
-                    await bot.edit_channel(before.voice_channel, name=re.sub(r'\(.*?\)', '', before.voice_channel.name))
-                    print("Removing eventual game tag from channel " + before.voice_channel.name + " because most played game is none")
-
-            # check if new one hasn't been named already
-            if not re.search(r'\(.*?\)', after.voice_channel.name):
-                if after.game:
-                    await bot.edit_channel(after.voice_channel, name=after.voice_channel.name + " (" + after.game.name + ")")
-                    print("Changing channel name to " + after.voice_channel.name + " (" + after.game.name + ")")
-            else:
-                to_name = getMostPlayedGameInChannel(after.voice_channel)
-                if to_name:
-                    await bot.edit_channel(after.voice_channel, name=re.sub(r'\(.*?\)', '(' + to_name + ')', after.voice_channel.name))
-                    print("Changing game tag from channel " + after.voice_channel.name + " to (" + to_name + ")")
-                else:
-                    await bot.edit_channel(after.voice_channel, name=re.sub(r'\(.*?\)', '', after.voice_channel.name))
-                    print("Removing eventual game tag from channel " + after.voice_channel.name + " because most played game is none")
-
-        elif before.voice_channel and not after.voice_channel:
-            print(after.display_name + " left voice channel: " + before.voice_channel.name)
-            # remove game from channel name when leaving and empty (the brackets)
-            if not before.voice_channel.voice_members:
-                await bot.edit_channel(before.voice_channel, name=re.sub(r'\(.*?\)', '', before.voice_channel.name))
-                print("Removing eventual game tag from channel " + before.voice_channel.name)
-            else:
-                to_name = getMostPlayedGameInChannel(before.voice_channel)
-                if to_name:
-                    if not re.search(r'\(.*?\)', before.voice_channel.name):
-                        await bot.edit_channel(before.voice_channel, name=before.voice_channel.name + " (" + before.game.name + ")")
-                        print("Changing channel name to " + before.voice_channel.name + " (" + to_name + ")")
-                    else:
-                        await bot.edit_channel(before.voice_channel, name=re.sub(r'\(.*?\)', '(' + to_name + ')', before.voice_channel.name))
-                        print("Changing game tag from " + before.voice_channel.name + " to (" + to_name + ")")
-                else:
-                    await bot.edit_channel(before.voice_channel, name=re.sub(r'\(.*?\)', '', before.voice_channel.name))
-                    print("Removing eventual game tag from channel " + before.voice_channel.name + " because most played game is none")
+        if not m:  # no game is set in the title
+            if member.activity:  # is playing a game
+                print(member.name + " changed activity status to " + member.activity.name)
+                await voice_channel.edit(name="{} ({})".format(voice_channel.name, member.activity.name))
+            else:  # is not playing a game anymore
+                print(
+                    "Warning: Someone changed from an activity to none but no channel game was set before.")
+        else:  # a game is set in the title
+            current_channel_name = m.group(1)
+            current_channel_game = m.group(2)
+            to_game = get_most_played_game_in_channel(voice_channel)
+            if to_game:
+                if not current_channel_game == to_game:
+                    await voice_channel.edit(name="{} ({})".format(current_channel_name, to_game))
+                    print("Updating channel {} game from {} to {}".format(current_channel_name, current_channel_game,
+                                                                          to_game))
+            else:  # nobody in the channel is playing a game anymore
+                await voice_channel.edit(name=current_channel_name)
+                print("Removing game from " + current_channel_name + " because most played game is none")
     except Forbidden:
-        print("Warning: Didn't have permission to change the channel name...")
+        print("Warning: Didn't have permission to change the channel name")
 
 
-def getMostPlayedGameInChannel(channel):
-    members = channel.voice_members
+def get_most_played_game_in_channel(channel):
+    members = channel.members
     gamelist = []
 
     for m in members:
-        if m.game:
-            gamelist.append(m.game.name)
+        if m.activity:
+            gamelist.append(m.activity.name)
     count = Counter(gamelist)
 
     first_most_common = None
@@ -160,8 +107,4 @@ def getMostPlayedGameInChannel(channel):
         return None
 
 
-def startup():
-    bot.run("your token here")
-
-
-startup()
+bot.run(TOKEN)
